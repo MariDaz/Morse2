d22eea2a97cdd4ff1b651eceab9d5088ea764144
Maria de los Angeles Diaz Berumen <mariadelosangeleesdiazberumen@gmail.com>
2024-09-28 16:45:11 -0700

    Agregar archivos de proyecto.

---
+++ Morse2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Morse2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
